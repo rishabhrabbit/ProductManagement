be32f241e93048c26fc73ba5005b1be5df20c884
rishabhrabbit <rishabh7095@gmail.com>
2021-08-17 13:51:31 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,50 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public Product saveProduct(Product product){
+        return repository.save(product);
+    }
+
+    public List<Product> saveProducts(List<Product> products){
+        return repository.saveAll(products);
+    }
+
+    public List<Product> getProducts(){
+        return repository.findAll();
+    }
+    public Product getProductById(int id){
+        return repository.findById(id).orElse(null);
+    }
+
+    public Product getProductByName(String name){
+        return repository.findByName(name);
+    }
+
+    public String deleteProduct(int id){
+        repository.deleteById(id);
+        return "Product removed with id: "+id;
+    }
+
+    public String updateProduct(Product product){
+        Product p = repository.findById(product.getId()).orElse(null);
+        p.setPrice(product.getPrice());
+        p.setName(product.getName());
+        p.setQuantity(product.getQuantity());
+        repository.save(p);
+        return "Product Updated with name: "+p;
+    }
+
+}
+
